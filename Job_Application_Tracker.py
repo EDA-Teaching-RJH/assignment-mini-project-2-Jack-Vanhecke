b9911ab3fc1b3583010f98fc594091f444d3defd
Jack-Vanhecke <jv11@kent.ac.uk>
2026-03-09 08:45:24 +0000

    Added comment to the end as a list of requirements

--- Job_Application_Tracker.py
+++ Job_Application_Tracker.py
@@ -4,3 +4,20 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+'''The program stores and manages job applications.
+
+Each application could contain:
+    Company Name
+    Job Title
+    Application Date
+    Status (Applied / Interview / Offer / Rejected)
+    Email Contact
+    Notes
+    
+Users should be able to:
+    Add a job application
+    Update status
+    Search applications
+    Save and load data
+    Validate inputs'''
