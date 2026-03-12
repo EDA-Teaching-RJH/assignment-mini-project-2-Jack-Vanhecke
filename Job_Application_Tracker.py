d57f7da98312b8df1241139833ce3028ec60890f
Jack-Vanhecke <jv11@kent.ac.uk>
2026-03-12 15:51:59 +0000

    Fixed the outputs from being boolean to the respective data value, and sorted out the date validation code

--- Job_Application_Tracker.py
+++ Job_Application_Tracker.py
@@ -21,22 +21,22 @@ class Opperations:
     def Validate_Company(self, company):                                    #Ensures the company name is not empty
         if company == "":
             print("Company name cannot be empty. Enter a valid company name.")
-            return True
-        return company
+            return company
+        return True
     
     def Validate_Job_Title(self, title):                                    #Ensures the job title is not empty
         if title == "":
             print("Job title cannot be empty. Enter a valid job title.")
-            return True
-        return title
+            return title
+        return True
     
     def Validate_Application_Date(self, date):                              #Ensures the application date is not empty and in a valid format
         if date == "":
             print("Application date cannot be empty. Enter a valid date in the format DD-MM-YYYY.")
-            return True
+            return date
         elif datetime.strptime(date, "%d-%m-%Y"):
-            return True
-        return date
+            return date
+        return True
         
 
 
@@ -45,7 +45,7 @@ class Opperations:
 
 
 
-        
+
                                                 # Blank to let me figure out how im working the dates
         print(datetime.datetime.now().strftime('%d/%m/%Y'))
 
@@ -54,16 +54,19 @@ class Opperations:
 
     def Validate_Status(self, status):                                      #Ensures the application Status is deemed as a valid status (Applied, Interview, Offer, Rejected)
         if re.search(r'^(Applied|Interview|Accepted|Rejected)$', status):
-            return True
+            return status
         elif status == "":
             print("Status cannot be empty. Enter a valid status (Applied / Interview / Offer / Rejected).")
-            return True
-        return status
+            return status
+        return True
     
     def Validate_Email(self, email):                                        # Ensures the email is a valid email
         if re.search(r'^[\w\.-]+@[\w\.-]+\.\w+$', email):
-            return True
-        return email
+            return email
+        elif email == "":
+            print("Email cannot be empty. Enter a valid email address.")
+            return email
+        return True
     
     def add_app(self):
         while True:
@@ -77,8 +80,12 @@ class Opperations:
                 break
 
         while True:
-            date = input("Application Date: ")
-            if self.Validate_Application_Date(date):
+            date = input("Did you apply today or another date? (Enter 'today' or a date in the format DD-MM-YYYY) ")
+            if date == "today":
+                date = datetime.now().strftime("%d-%m-%Y")
+                break
+            elif self.Validate_Application_Date(date):
+                date = self.Validate_Application_Date(date)
                 break
 
         while True:
@@ -92,8 +99,8 @@ class Opperations:
             if self.Validate_Email(email):
                 break
             
-        while True:
-            notes = input("Notes: ")
+        
+        notes = input("Notes: ")
 
         appended_app = Application_Format(company, title, date, status, email, notes)
         self.application.append(appended_app)
