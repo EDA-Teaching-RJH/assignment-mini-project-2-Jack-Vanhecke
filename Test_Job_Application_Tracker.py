8528d018f20c31ba0aec9bfca4f76ab25b925563
Jack-Vanhecke <jv11@kent.ac.uk>
2026-03-14 00:26:40 +0000

    Removed Job Applications data and started test function for company validation

--- Test_Job_Application_Tracker.py
+++ Test_Job_Application_Tracker.py
@@ -1,5 +1,17 @@
-def main():
+import Job_Application_Tracker
 
-    print("Hello World")
+operations = Job_Application_Tracker.Operations()
 
-main()
+def test_validate_company():
+
+    try:    # Test valid company name
+        assert operations.Validate_Company("Google") == True
+    except AssertionError:
+        print("Valid company name failed")
+
+    try:    # Test invalid company name
+        assert operations.Validate_Company("") == False
+    except AssertionError:
+        print("Should fail")
+
+test_validate_company()
